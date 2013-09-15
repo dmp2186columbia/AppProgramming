f025a3f380d825b63ec674b7a8c0cdf84fdc85c8
dmp2186columbia <dmp2186@columbia.edu>
2013-09-14 22:34:59 -0400

    Added switches

--- FinLab/Main.cpp
+++ FinLab/Main.cpp
@@ -6,39 +6,44 @@
 
 void exercise1(void)
 {
-	std::cout<<"lol";
+	std::cout<<"Exercise 1";
 }
 
-/* other exercises here
 void exercise2(void)
 {
-	std::cout<<"lol";
+	std::cout<<"Exercise 2";
 }
 
 void exercise3(void)
 {
-	std::cout<<"lol";
+	std::cout<<"Exercise 3";
 }
 
 void exercise4(void)
 {
-	std::cout<<"lol";
+	std::cout<<"Exercise 4";
 }
-*/
 int main () 
 {
-	int select;
+	/*int select;
+	std::cout << "Select an exercise to run (1-4):\n";
+	std::cin >> select;
 	switch (select)
 	{
-	case '1' : exercise1;
+	case 1 : exercise1();
 		break;
-	/*case '2' : exercise1;
+	case 2 : exercise2();
 		break;
-	case '3' : exercise1;
+	case 3 : exercise3();
 		break;
-	case '4' : exercise1;
-		break;*/
-	}
+	case 4 : exercise4();
+		break;
+	default : std::cout<< "some error happened";
+		break;
+	}*/
+	
+	return 0;
+	
 }
 
 
