cb09dc49bd36618e352043b0160e93bcdbbf2b9c
hackerJ <jl4005@columbia.edu>
2013-09-25 18:01:00 -0700

    change init msg display

--- FinLab/Main.cpp
+++ FinLab/Main.cpp
@@ -238,10 +238,11 @@ void exercise3(void)
 
 int main () 
 {
-	std::cout<<"====================================================="<<std::endl;
-	std::cout<<"====== FINLAB OPTION PRICING PROGRAM VER 0.0.0 ======"<<std::endl;
+	
 	for (EVER)
 	{
+		std::cout<<"====================================================="<<std::endl;
+		std::cout<<"====== FINLAB OPTION PRICING PROGRAM VER 0.0.0 ======"<<std::endl;
 		char selection = menu();
 		switch (selection)
 		{
@@ -263,6 +264,7 @@ int main ()
 		}
 		std::cin.clear();
 		std::cin.ignore(100, '\n');
+		std::cout<<std::endl;
 	}
 	return 0;
 }
